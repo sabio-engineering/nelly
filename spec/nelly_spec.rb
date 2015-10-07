728ed426dc24a5cad69590eef61f585e5a9534f9
Viktor Fonic <viktor.fonic@gmail.com>
2015-10-07 15:01:08 +0100

    Use more generic names for tested classes in specs

--- spec/nelly_spec.rb
+++ spec/nelly_spec.rb
@@ -2,30 +2,28 @@ require 'nelly'
 require 'spec_helper'
 require 'support/db/active_record'
 
-class Post < ActiveRecord::Base
+class Cacheable < ActiveRecord::Base
   acts_as_cacheable
 end
 
-class Comment < ActiveRecord::Base
+class NonCacheable < ActiveRecord::Base
 end
 
-describe Post do
+describe Cacheable do
   describe '.cache_key' do
     let(:updated_at) { Time.now }
-    let!(:cacheable_object) { Post.create!(updated_at: updated_at) }
+    let!(:cacheable_object) { Cacheable.create!(updated_at: updated_at) }
 
-    subject { Post.cache_key }
+    it { expect(described_class).to respond_to(:cache_key) }
 
     it 'returns a cache key' do
       max_updated_at = updated_at.utc.to_s(:number)
-      count = Post.count
-      expect(subject).to eq("Post-#{count}-#{max_updated_at}")
+      count = Cacheable.count
+      expect(described_class.cache_key).to eq("Cacheable-#{count}-#{max_updated_at}")
     end
   end
 end
 
-describe Comment do
-  it 'does not act as cacheable' do
-    expect(Comment).to_not respond_to(:cache_key)
-  end
+describe NonCacheable do
+  it { expect(described_class).to_not respond_to(:cache_key) }
 end
--- spec/support/db/schema.rb
+++ spec/support/db/schema.rb
@@ -1,7 +1,7 @@
 ActiveRecord::Schema.define do
   self.verbose = false
 
-  create_table :posts, force: true do |t|
+  create_table :cacheables, force: true do |t|
     t.timestamps null: false
   end
 end
